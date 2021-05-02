30a5a311e1b212842729e1dd90d55a17eb5122c4
FolenHealer <folenrey@gmail.com>
2021-05-02 14:12:32 +0300

    Create more styles for auth theme and end forgot screen

--- bagzz_app_ui/lib/theme/auth_theme.dart
+++ bagzz_app_ui/lib/theme/auth_theme.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 
 abstract class AuthTheme {
+  // TITLES AUTH THEME
   static const titleTextStyle = TextStyle(
     color: Colors.black,
     fontSize: 24,
     fontFamily: 'Work Sans',
     fontWeight: FontWeight.w500,
   );
+  static const subtitleTextStyle = TextStyle(
+    color: Colors.black,
+    fontSize: 9,
+    fontFamily: 'Work Sans',
+    fontWeight: FontWeight.w500,
+  );
   static const titleTextAlign = TextAlign.center;
+  static const subtitleTextAlign = TextAlign.center;
+
+  // TEXT FIELD AUTH THEME
   static const textfieldInputBorder = OutlineInputBorder(
     borderSide: BorderSide(
       color: Colors.black,
@@ -21,4 +31,18 @@ abstract class AuthTheme {
     fontWeight: FontWeight.w500,
   );
   static const textfieldContetPadding = EdgeInsets.all(8.0);
+
+  // SUBMIT BUTTON AUTH THEME(LOGIN, etc.)
+  static final submitButtonStyle = ButtonStyle(
+    backgroundColor: MaterialStateProperty.all<Color?>(Colors.black),
+    padding: MaterialStateProperty.all<EdgeInsetsGeometry?>(
+        EdgeInsets.symmetric(horizontal: 70, vertical: 10)),
+  );
+  static const sumbitButtonTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 16,
+    fontFamily: 'Work Sans',
+    fontWeight: FontWeight.w500,
+  );
+  static const sumbitButtonTextAlign = TextAlign.center;
 }
